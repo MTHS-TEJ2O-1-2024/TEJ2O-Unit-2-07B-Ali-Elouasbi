ce76b7df3a7474311767fb53b0c8754537ae907a
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-02 19:40:13 -0400

    2-07

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ali Elouasbi
+ * Created on: Oct 2024
+ * This program plays a Cookie Clicker game
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+let nombre = 1
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(nombre)
+    nombre = nombre + 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(0)
+    nombre = 1
+})
